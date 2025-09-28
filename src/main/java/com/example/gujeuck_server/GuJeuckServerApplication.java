eec09b95082823908edbf8fbc9a76af0133e121b
박태수 <taesupark0504@gmail.com>
2025-09-28 19:18:08 +0900

    chore :: 기본 세팅

---
+++ src/main/java/com/example/gujeuck_server/GuJeuckServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gujeuck_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuJeuckServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuJeuckServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gujeuck_server/GuJeuckServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gujeuck_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuJeuckServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
