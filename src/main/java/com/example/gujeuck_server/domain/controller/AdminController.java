94657a790a41a7c3712017a2ce020edcdf67b0da
oijwef098234 <oijwef098234@gmail.com>
2025-10-01 02:39:14 +0900

    feat : 방문 목적 추가

--- src/main/java/com/example/gujeuck_server/domain/controller/AdminController.java
+++ src/main/java/com/example/gujeuck_server/domain/controller/AdminController.java
@@ -3,6 +3,7 @@ package com.example.gujeuck_server.domain.controller;
 import com.example.gujeuck_server.domain.entity.Purpose;
 import com.example.gujeuck_server.domain.service.CreatePurpose;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
     private final CreatePurpose createPurpose;
 
+    @PostMapping("/purpose/create")
     public void createPurpose(Purpose purpose) {
         createPurpose.createPurpose(purpose);
     }
