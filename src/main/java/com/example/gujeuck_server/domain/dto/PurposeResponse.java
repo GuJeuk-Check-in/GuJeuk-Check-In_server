0b6e362730b66c142f18751e52522fe7e0b7f7c8
oijwef098234 <oijwef098234@gmail.com>
2025-10-03 21:11:48 +0900

    feat : 코드 리뷰 반영

--- src/main/java/com/example/gujeuck_server/domain/dto/PurposeResponse.java
+++ src/main/java/com/example/gujeuck_server/domain/dto/PurposeResponse.java
@@ -1,10 +1,10 @@
 package com.example.gujeuck_server.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PurposeResponse {
