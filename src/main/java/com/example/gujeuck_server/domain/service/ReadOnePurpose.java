d0bc21a653d3169263e125844143b278707edf75
oijwef098234 <oijwef098234@gmail.com>
2025-10-03 17:22:42 +0900

    feat : 코드 리뷰 반영

--- src/main/java/com/example/gujeuck_server/domain/service/ReadOnePurpose.java
+++ src/main/java/com/example/gujeuck_server/domain/service/ReadOnePurpose.java
@@ -1,6 +1,7 @@
 package com.example.gujeuck_server.domain.service;
 
 import com.example.gujeuck_server.domain.entity.Purpose;
+import com.example.gujeuck_server.domain.exception.NotFoundPurposeException;
 import com.example.gujeuck_server.domain.repository.PurposeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ public class ReadOnePurpose {
     private final PurposeRepository purposeRepository;
 
     public Purpose readById(Long id) {
-        return purposeRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 방문목적입니다."));
+        return purposeRepository.findById(id).orElseThrow(
+                () -> NotFoundPurposeException.EXCEPTION);
     }
 }
