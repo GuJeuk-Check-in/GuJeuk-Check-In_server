97a33aa87b5b3a689da813489f6b0b6f266ce136
oijwef098234 <oijwef098234@gmail.com>
2025-10-04 19:53:33 +0900

    feat : 방문 목적 삭제 예외처리 추가

--- src/main/java/com/example/gujeuck_server/domain/service/DeletePurposeService.java
+++ src/main/java/com/example/gujeuck_server/domain/service/DeletePurposeService.java
@@ -1,5 +1,6 @@
 package com.example.gujeuck_server.domain.service;
 
+import com.example.gujeuck_server.domain.exception.NotFoundPurposeException;
 import com.example.gujeuck_server.domain.repository.PurposeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,9 @@ public class DeletePurposeService {
     private final PurposeRepository purposeRepository;
 
     public void deletePurpose(Long id) {
+        purposeRepository.findById(id).orElseThrow(
+                () -> NotFoundPurposeException.EXCEPTION
+        );
         purposeRepository.deleteById(id);
     }
 }
