10da0cf510ce2f04f86e939b364e49a66bd3d9eb
oijwef098234 <oijwef098234@gmail.com>
2025-10-04 22:39:11 +0900

    feat : 트렌젝션 어노테이션 추가

--- src/main/java/com/example/gujeuck_server/domain/service/DeletePurposeService.java
+++ src/main/java/com/example/gujeuck_server/domain/service/DeletePurposeService.java
@@ -1,19 +1,22 @@
 package com.example.gujeuck_server.domain.service;
 
+import com.example.gujeuck_server.domain.entity.Purpose;
 import com.example.gujeuck_server.domain.exception.NotFoundPurposeException;
 import com.example.gujeuck_server.domain.repository.PurposeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class DeletePurposeService {
     private final PurposeRepository purposeRepository;
 
+    @Transactional
     public void deletePurpose(Long id) {
-        purposeRepository.findById(id).orElseThrow(
+        Purpose purpose = purposeRepository.findById(id).orElseThrow(
                 () -> NotFoundPurposeException.EXCEPTION
         );
-        purposeRepository.deleteById(id);
+        purposeRepository.delete(purpose);
     }
 }
