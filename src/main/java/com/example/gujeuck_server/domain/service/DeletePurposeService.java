7626fccbfdffcc38effdd181c0118c2b93d8133e
oijwef098234 <oijwef098234@gmail.com>
2025-10-04 19:48:54 +0900

    feat : 트렌젝션 어노테이션 추가

--- src/main/java/com/example/gujeuck_server/domain/service/DeletePurposeService.java
+++ src/main/java/com/example/gujeuck_server/domain/service/DeletePurposeService.java
@@ -3,8 +3,10 @@ package com.example.gujeuck_server.domain.service;
 import com.example.gujeuck_server.domain.repository.PurposeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class DeletePurposeService {
     private final PurposeRepository purposeRepository;
