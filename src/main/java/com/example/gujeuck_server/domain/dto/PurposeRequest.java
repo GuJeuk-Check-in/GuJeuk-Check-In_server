ba4691933b5bbb441c7e9729c90d1cab7a561aa4
oijwef098234 <oijwef098234@gmail.com>
2025-10-05 01:34:05 +0900

    feat : @NotBlank 어노테이션 추가

--- src/main/java/com/example/gujeuck_server/domain/dto/PurposeRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/dto/PurposeRequest.java
@@ -1,5 +1,6 @@
 package com.example.gujeuck_server.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import lombok.NoArgsConstructor;
 public class PurposeRequest {
 
     @Size(min = 1, max = 30)
+    @NotBlank
     private String purpose;
+
+    @NotBlank
     private String purposeImage;
 }
