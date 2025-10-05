73429bd64282c203050aea5495dafa2848c7d54d
oijwef098234 <oijwef098234@gmail.com>
2025-10-05 17:19:33 +0900

    feat : 회원 목록 단일 조회

--- src/main/java/com/example/gujeuck_server/domain/User/controller/UserController.java
+++ src/main/java/com/example/gujeuck_server/domain/User/controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.gujeuck_server.domain.User.controller;
 
 import com.example.gujeuck_server.domain.User.dto.UserResponse;
 import com.example.gujeuck_server.domain.User.service.ReadAllUserListService;
+import com.example.gujeuck_server.domain.User.service.ReadOneUserListService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,15 +13,15 @@ import java.util.List;
 @RequestMapping("/admin/user")
 public class UserController {
     private final ReadAllUserListService readAllUserListService;
-    private final TestCreateUserListService testCreateUserListService;
-
-    @PostMapping("/create")
-    public void testCreatePurpose(@RequestBody UserRequest userRequest) {
-        testCreateUserListService.createUserList(userRequest);
-    }
+    private final ReadOneUserListService readOneUserListService;
 
     @GetMapping("/all")
     public List<UserResponse> getAllUserList() {
         return readAllUserListService.readAllUserList();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponse getOneUserList(@PathVariable Long id) {
+        return readOneUserListService.readOneUserList(id);
     }
 }
