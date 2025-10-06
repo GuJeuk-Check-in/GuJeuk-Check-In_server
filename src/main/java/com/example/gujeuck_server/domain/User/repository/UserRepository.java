03c0514e51f70fda1977cc32d8b33555b0266a1a
박태수 <taesupark0504@gmail.com>
2025-10-06 18:28:13 +0900

    chore :: user 소문자 처리

--- src/main/java/com/example/gujeuck_server/domain/User/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gujeuck_server.domain.User.repository;
-
-import com.example.gujeuck_server.domain.User.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
