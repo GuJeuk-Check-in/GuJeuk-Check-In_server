83abd5d5b73ced7ee87b1e3d2f2fccbc459217f3
박태수 <taesupark0504@gmail.com>
2025-10-06 18:29:05 +0900

    chore :: log 소문자 처리

--- src/main/java/com/example/gujeuck_server/domain/Log/repository/LogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gujeuck_server.domain.Log.repository;
-
-import com.example.gujeuck_server.domain.Log.entity.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Long> {
-
-}
