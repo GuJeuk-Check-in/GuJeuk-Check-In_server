4cfe6ef7ae6995a97cbf481ea7d6ac6f70c093ec
oijwef098234 <oijwef098234@gmail.com>
2025-10-10 13:24:31 +0900

    feat : 유효성 검사 추가

--- src/main/java/com/example/gujeuck_server/domain/admin/dto/UseListRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/admin/dto/UseListRequest.java
@@ -1,5 +1,6 @@
 package com.example.gujeuck_server.domain.admin.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,15 +9,25 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 public class UseListRequest {
+
+    @NotBlank(message = "이름은 빈칸으로 둘 수 없습니다.")
     private String name;
+
+    @NotBlank(message = "나이를 선택해주세요")
     private String age;
+
+    @NotBlank(message = "전화번호를 입력해주세요")
     private String phone;
 
+    @NotBlank(message = "남, 여 인원수는 1명 이상 선택해야합니다.")
     private int maleCount;
 
+    @NotBlank(message = "남, 여 인원수는 1명 이상 선택해야합니다.")
     private int femaleCount;
 
+    @NotBlank(message = "발문목적을 선택해주세요.")
     private Long purposeId;
 
+    @NotBlank(message = "방문날짜를 선택해주세요.")
     private LocalDate visitDate;
 }
