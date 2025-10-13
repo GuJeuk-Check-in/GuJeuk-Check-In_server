2454d89f4a213723f66c513aa844376da74bbd88
박태수 <taesupark0504@gmail.com>
2025-10-13 18:56:49 +0900

    feat :: findByPurpose

--- src/main/java/com/example/gujeuck_server/domain/purpose/repository/PurposeRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/purpose/repository/PurposeRepository.java
@@ -3,6 +3,8 @@ package com.example.gujeuck_server.domain.purpose.repository;
 import com.example.gujeuck_server.domain.purpose.entity.Purpose;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PurposeRepository extends JpaRepository<Purpose, Long> {
+import java.util.Optional;
 
+public interface PurposeRepository extends JpaRepository<Purpose, Long> {
+    Optional<Purpose> findByPurpose(String purpose);
 }
