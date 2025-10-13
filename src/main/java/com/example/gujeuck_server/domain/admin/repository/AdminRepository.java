f84efa28c903662358301cd34b539975ef98e325
박태수 <taesupark0504@gmail.com>
2025-10-13 23:35:05 +0900

    feat :: findByPassword

--- src/main/java/com/example/gujeuck_server/domain/admin/repository/AdminRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/admin/repository/AdminRepository.java
@@ -3,6 +3,10 @@ package com.example.gujeuck_server.domain.admin.repository;
 import com.example.gujeuck_server.domain.admin.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin> findByPassword(String password);
 
 }
