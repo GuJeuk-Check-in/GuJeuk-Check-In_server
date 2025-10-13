7f9f8d06d197d3a05b3f5668287df486ae12c676
박태수 <taesupark0504@gmail.com>
2025-10-13 18:56:27 +0900

    feat :: 날짜 포맷과 목적 엔티티 적용

--- src/main/java/com/example/gujeuck_server/domain/user/service/SignupService.java
+++ src/main/java/com/example/gujeuck_server/domain/user/service/SignupService.java
@@ -1,14 +1,22 @@
 package com.example.gujeuck_server.domain.user.service;
 
+import com.example.gujeuck_server.domain.log.entity.Log;
 import com.example.gujeuck_server.domain.log.repository.LogRepository;
+import com.example.gujeuck_server.domain.purpose.entity.Purpose;
+import com.example.gujeuck_server.domain.purpose.exception.PurposeNotFoundException;
+import com.example.gujeuck_server.domain.purpose.repository.PurposeRepository;
 import com.example.gujeuck_server.domain.user.entity.User;
 import com.example.gujeuck_server.domain.user.entity.enums.Age;
 import com.example.gujeuck_server.domain.user.exception.ExistUserIdException;
 import com.example.gujeuck_server.domain.user.repository.UserRepository;
 import com.example.gujeuck_server.domain.user.dto.request.SignupRequest;
+import com.example.gujeuck_server.global.utility.CalculateAgeService;
+import com.example.gujeuck_server.global.utility.DateFormatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +24,7 @@ public class SignupService {
     private final UserRepository userRepository;
     private final CalculateAgeService calculateAgeService;
     private final LogRepository logRepository;
+    private final PurposeRepository purposeRepository;
 
     @Transactional
     public void signup(SignupRequest request) {
@@ -27,6 +36,11 @@ public class SignupService {
 
         Age age = calculateAgeService.getAge(request.getBirthYMD());
 
+        String formattedDate = DateFormatter.LocalDateForm(LocalDate.now());
+
+        Purpose purpose = purposeRepository.findByPurpose(request.getPurpose())
+                        .orElseThrow(() -> PurposeNotFoundException.EXCEPTION);
+
         userRepository.save(User.builder()
                 .name(request.getName())
                 .phone(request.getPhone())
@@ -36,6 +50,15 @@ public class SignupService {
                 .privacyAgreed(request.isPrivacyAgreed())
                 .age(age)
                 .userId(userId)
+                .build());
+
+        logRepository.save(Log.builder()
+                .name(request.getName())
+                .phone(request.getPhone())
+                .age(age)
+                .purpose(purpose.getPurpose())
+                .privacyAgreed(request.isPrivacyAgreed())
+                .visitDate(formattedDate)
                 .build());
     }
 }
