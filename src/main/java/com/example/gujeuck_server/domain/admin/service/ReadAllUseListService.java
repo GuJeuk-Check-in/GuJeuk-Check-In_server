2c466e902406f11d74c4934ae65276ad64650b7b
oijwef098234 <oijwef098234@gmail.com>
2025-10-13 19:03:48 +0900

    feat :: 이용기록 수정 전체 조회

---
+++ src/main/java/com/example/gujeuck_server/domain/admin/service/ReadAllUseListService.java
@@ -0,0 +1,20 @@
+package com.example.gujeuck_server.domain.admin.service;
+
+import com.example.gujeuck_server.domain.admin.dto.UseListResponse;
+import com.example.gujeuck_server.domain.log.repository.LogRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ReadAllUseListService {
+    private final LogRepository logRepository;
+
+    public List<UseListResponse> readAllUseList() {
+        return logRepository.findAll().stream()
+                .map(log -> new UseListResponse(log.getId(), log.getName(), log.getAge(), log.getPhone(), log.getMaleCount(), log.getMaleCount(), log.getPurpose().getId(), log.getVisitDate(), log.isPrivacyAgreed()))
+                .toList();
+    }
+}
