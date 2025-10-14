0198514e89826ac1b810624ed439576d56056a1a
박태수 <taesupark0504@gmail.com>
2025-10-14 11:19:06 +0900

    feat :: QLog에 year필드 추가

--- src/main/generated/com/example/gujeuck_server/domain/log/entity/QLog.java
+++ src/main/generated/com/example/gujeuck_server/domain/log/entity/QLog.java
@@ -37,6 +37,8 @@ public class QLog extends EntityPathBase<Log> {
 
     public final StringPath visitDate = createString("visitDate");
 
+    public final NumberPath<Integer> year = createNumber("year", Integer.class);
+
     public QLog(String variable) {
         super(Log.class, forVariable(variable));
     }
