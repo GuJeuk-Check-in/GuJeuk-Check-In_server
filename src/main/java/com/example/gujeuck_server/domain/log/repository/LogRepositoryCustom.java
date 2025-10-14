fc41141cef27f92146e3bdb52353326cbcd053ee
박태수 <taesupark0504@gmail.com>
2025-10-14 11:02:27 +0900

    feat :: LogRepositorycustom

---
+++ src/main/java/com/example/gujeuck_server/domain/log/repository/LogRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.example.gujeuck_server.domain.log.repository;
+
+public interface LogRepositoryCustom {
+}
---
+++ src/main/java/com/example/gujeuck_server/domain/log/repository/LogRepositoryCustomImpl.java
@@ -0,0 +1,7 @@
+package com.example.gujeuck_server.domain.log.repository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class LogRepositoryCustomImpl implements LogRepositoryCustom {
+}
