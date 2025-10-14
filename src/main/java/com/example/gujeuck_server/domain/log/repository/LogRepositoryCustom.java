ba229855460ae75093df2e29d652f18a353fb5b9
박태수 <taesupark0504@gmail.com>
2025-10-14 12:06:03 +0900

    feat :: findAllByCurrentMonth구현

--- src/main/java/com/example/gujeuck_server/domain/log/repository/LogRepositoryCustom.java
+++ src/main/java/com/example/gujeuck_server/domain/log/repository/LogRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.gujeuck_server.domain.log.repository;
 
+import com.example.gujeuck_server.domain.log.dto.response.LogResponse;
+
+import java.util.List;
+
 public interface LogRepositoryCustom {
+    List<LogResponse> findAllByCurrentMonth();
 }
