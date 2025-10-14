4b29f1516f0a06ffd4cb0d9d8f1625ac639b5db2
박태수 <taesupark0504@gmail.com>
2025-10-14 15:57:04 +0900

    refactor :: 400 -> 401

--- src/main/java/com/example/gujeuck_server/global/error/exception/ErrorCode.java
+++ src/main/java/com/example/gujeuck_server/global/error/exception/ErrorCode.java
@@ -34,7 +34,7 @@ public enum ErrorCode {
 
     //admin
     ADMIN_NOT_FOUND(404, "해당 관리자 계정이 존재하지 않습니다."),
-    INVALID_PASSWORD_CONFIRM(400, "비밀번호 확인이 일치하지 않습니다."),
+    INVALID_PASSWORD_CONFIRM(401, "비밀번호 확인이 일치하지 않습니다."),
     SAME_OLD_PASSWORD(400, "기존 비밀번호와 동일한 비밀번호로 변경할 수 없습니다."),
 
     //s3
