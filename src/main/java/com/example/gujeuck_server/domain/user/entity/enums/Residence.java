8e9c4c08485b6a38153d51d93ccf039acac08fd8
박태수 <taesupark0504@gmail.com>
2025-10-15 08:39:39 +0900

    feat :: 거주지 enum 클래스

---
+++ src/main/java/com/example/gujeuck_server/domain/user/entity/enums/Residence.java
@@ -0,0 +1,20 @@
+package com.example.gujeuck_server.domain.user.entity.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Residence {
+    GWANPYEONG_DONG,
+    GUJEOK_DONG,
+    NOEUN_1_DONG,
+    NOEUN_2_DONG,
+    NOEUN_3_DONG,
+    SANDAE_DONG,
+    SHINSEONG_DONG,
+    ONCHEON_1_DONG,
+    WONSINHEUNG_DONG,
+    JEONMIN_DONG,
+    JINJAM_DONG,
+    HAKHA_DONG,
+    ETC
+}
