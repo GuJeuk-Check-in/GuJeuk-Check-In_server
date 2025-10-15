415be9fd0203406d474b0244a72b215e6f9ffccb
oijwef098234 <oijwef098234@gmail.com>
2025-10-15 22:49:20 +0900

    feat :: adminFacade구현

--- src/main/java/com/example/gujeuck_server/domain/admin/service/list/ReadOneUseListService.java
+++ src/main/java/com/example/gujeuck_server/domain/admin/service/list/ReadOneUseListService.java
@@ -2,6 +2,7 @@ package com.example.gujeuck_server.domain.admin.service.list;
 
 import com.example.gujeuck_server.domain.admin.dto.response.UseListResponse;
 import com.example.gujeuck_server.domain.admin.exception.LogNotFountException;
+import com.example.gujeuck_server.domain.admin.facade.AdminFacade;
 import com.example.gujeuck_server.domain.log.entity.Log;
 import com.example.gujeuck_server.domain.log.repository.LogRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +12,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ReadOneUseListService {
     private final LogRepository logRepository;
+    private final AdminFacade adminFacade;
 
     public UseListResponse readOneUseList(Long id) {
+        adminFacade.currentUser();
+
         Log log = logRepository.findById(id).orElseThrow(
                 () -> LogNotFountException.EXCEPTION
         );
