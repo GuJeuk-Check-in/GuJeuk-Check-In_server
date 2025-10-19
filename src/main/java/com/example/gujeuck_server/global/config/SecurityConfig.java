cb70d35bfe37f4dc47d0516081ac097e78671751
oijwef098234 <oijwef098234@gmail.com>
2025-10-19 16:44:10 +0900

    feat :: 바꾸니 baseURL 허용

--- src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
+++ src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
@@ -45,8 +45,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "https://gujeuk.dsmhs.kr",
-                "http://localhost:5173"
+                "https://gujeuk.dsmhs.kr", "https://gu-jeuk-check-in.vercel.app"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
