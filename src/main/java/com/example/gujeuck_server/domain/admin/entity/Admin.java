1f8c727344ef5fe1a5566591d257e50a88164b10
박태수 <taesupark0504@gmail.com>
2025-10-23 11:46:04 +0900

    chore :: 와일드카드 빼고

--- src/main/java/com/example/gujeuck_server/domain/admin/entity/Admin.java
+++ src/main/java/com/example/gujeuck_server/domain/admin/entity/Admin.java
@@ -1,7 +1,11 @@
 package com.example.gujeuck_server.domain.admin.entity;
 
+
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
