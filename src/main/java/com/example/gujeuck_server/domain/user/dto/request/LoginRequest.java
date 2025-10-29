bb8ce67d0429e56492b3d5f552ed64d7ef6db1c3
박태수 <taesupark0504@gmail.com>
2025-10-29 23:29:32 +0900

    chore :: test용 생성자

--- src/main/java/com/example/gujeuck_server/domain/user/dto/request/LoginRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/user/dto/request/LoginRequest.java
@@ -1,11 +1,15 @@
 package com.example.gujeuck_server.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
 
     @NotBlank
