84024b34192294d5ec106a71fc07c7994306661a
박태수 <taesupark0504@gmail.com>
2025-10-29 19:14:12 +0900

    refactor :: userRepositoryCustom 상속

--- src/main/java/com/example/gujeuck_server/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/user/repository/UserRepository.java
@@ -6,11 +6,8 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByUserId(String userId);
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
     Boolean existsByUserId(String userId);
 
