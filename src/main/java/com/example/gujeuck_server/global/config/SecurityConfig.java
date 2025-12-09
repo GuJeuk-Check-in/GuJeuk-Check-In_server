7698a60583d333a6c64f153702ec7c50dc73b45e
oijwef098234 <oijwef098234@gmail.com>
2025-12-09 22:24:46 +0900

    feat :: CORS주소 변경

--- src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
+++ src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "https://gujeuk.dsmhs.kr", "https://gu-jeuk-check-in.vercel.app", "http://localhost:5173/"
+                "https://gujeuk.dsmhs.kr", "https://gu-jeuk-check-in.vercel.app", "http://localhost:5173"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"));
