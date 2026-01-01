e6fc88c4c3a2891d4b3c35f9974d2639510ff7a3
박태수 <taesupark0504@gmail.com>
2026-01-02 06:02:39 +0900

    refactor :: delete로 삭제

--- src/main/java/com/example/gujeuck_server/domain/admin/service/test/DeleteAllThingService.java
+++ src/main/java/com/example/gujeuck_server/domain/admin/service/test/DeleteAllThingService.java
@@ -19,6 +19,8 @@ public class DeleteAllThingService {
         adminFacade.currentUser();
 
         logRepository.deleteAll();
-        userRepository.deleteAll();
+
+        userRepository.findAll()
+                .forEach(userRepository::delete);
     }
 }
