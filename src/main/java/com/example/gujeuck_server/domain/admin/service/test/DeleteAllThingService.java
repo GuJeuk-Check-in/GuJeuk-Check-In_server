3828c65b422511e4c0e26cf63a573752b8d9986d
박태수 <taesupark0504@gmail.com>
2026-01-02 05:53:41 +0900

    feat :: 유저도 다 삭제

--- src/main/java/com/example/gujeuck_server/domain/admin/service/test/DeleteAllThingService.java
+++ src/main/java/com/example/gujeuck_server/domain/admin/service/test/DeleteAllThingService.java
@@ -1,7 +1,8 @@
-package com.example.gujeuck_server.domain.admin.service.list;
+package com.example.gujeuck_server.domain.admin.service.test;
 
 import com.example.gujeuck_server.domain.admin.facade.AdminFacade;
 import com.example.gujeuck_server.domain.log.repository.LogRepository;
+import com.example.gujeuck_server.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,11 +12,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeleteAllThingService {
     private final AdminFacade adminFacade;
     private final LogRepository logRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public void deleteALl() {
         adminFacade.currentUser();
 
         logRepository.deleteAll();
+        userRepository.deleteAll();
     }
 }
