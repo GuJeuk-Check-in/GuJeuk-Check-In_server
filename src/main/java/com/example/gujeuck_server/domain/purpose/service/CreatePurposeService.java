2b2a9ff5e6e0e65fb16fd5b63a2ccfb81e468241
oijwef098234 <oijwef098234@gmail.com>
2026-01-09 17:05:35 +0900

    feat :: 방문목적 생성 메서드 인덱스 증가 구현

--- src/main/java/com/example/gujeuck_server/domain/purpose/service/CreatePurposeService.java
+++ src/main/java/com/example/gujeuck_server/domain/purpose/service/CreatePurposeService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CreatePurposeService {
@@ -19,10 +21,12 @@ public class CreatePurposeService {
 
         adminFacade.currentUser();
 
+        List<Purpose> purposes = purposeRepository.findAll();
+
         purposeRepository.save(Purpose.builder()
                 .id(purpose.getId())
                 .purpose(purpose.getPurpose())
-                .indexId(purpose.getIndexId())
+                .index(purposes.size() + 1)
                 .build());
     }
-}
+} 
