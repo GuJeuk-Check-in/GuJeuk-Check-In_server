d928031e4524e1c8e622de2097106fc4bf37fff3
YoonJiwon <gaden119000@gmail.com>
2026-01-11 20:47:09 +0900

    @Transactional 추가

--- src/main/java/com/example/gujeuck_server/domain/user/service/QueryUserDetailService.java
+++ src/main/java/com/example/gujeuck_server/domain/user/service/QueryUserDetailService.java
@@ -5,12 +5,14 @@ import com.example.gujeuck_server.domain.user.domain.repository.UserRepository;
 import com.example.gujeuck_server.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class QueryUserDetailService {
     private final UserRepository userRepository;
 
+    @Transactional(readOnly = true)
     public User execute(final Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> UserNotFoundException.EXCEPTION);
