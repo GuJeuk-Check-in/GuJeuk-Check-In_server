1052bb6a8cb68f82db195734762b82633415225d
YoonJiwon <gaden119000@gmail.com>
2026-01-12 16:21:44 +0900

    매개변수 추가

--- src/main/java/com/example/gujeuck_server/domain/user/service/UpdateUserService.java
+++ src/main/java/com/example/gujeuck_server/domain/user/service/UpdateUserService.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Service;
 public class UpdateUserService {
     private final UserRepository userRepository;
 
-    public void execute(UpdateUserRequest request) {
+    public void execute(Long id, UpdateUserRequest request) {
         // 유저를 데이터베이스에서 id로 조회
-            // 유저가 없다면 오류
+        // 유저가 없다면 오류
         // request 값으로 해당 유저의 정보를 수정
             // 수정 요청된 userId가 다른 유저와 중복되면 오류
     }
