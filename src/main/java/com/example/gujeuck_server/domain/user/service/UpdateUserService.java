f84337c8054a35ae7861f3ab9cee670615446e22
YoonJiwon <gaden119000@gmail.com>
2026-01-12 16:13:47 +0900

    UserRepository 주입 및 주석과 어노테이션 추가

--- src/main/java/com/example/gujeuck_server/domain/user/service/UpdateUserService.java
+++ src/main/java/com/example/gujeuck_server/domain/user/service/UpdateUserService.java
@@ -1,12 +1,19 @@
 package com.example.gujeuck_server.domain.user.service;
 
+import com.example.gujeuck_server.domain.user.domain.repository.UserRepository;
 import com.example.gujeuck_server.domain.user.presentation.dto.request.UpdateUserRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UpdateUserService {
+    private final UserRepository userRepository;
 
     public void execute(UpdateUserRequest request) {
-
+        // 유저를 데이터베이스에서 id로 조회
+            // 유저가 없다면 오류
+        // request 값으로 해당 유저의 정보를 수정
+            // 수정 요청된 userId가 다른 유저와 중복되면 오류
     }
 }
