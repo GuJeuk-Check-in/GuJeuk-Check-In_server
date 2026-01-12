4cdeee282876a15f1398db5974e3650293224f96
YoonJiwon <gaden119000@gmail.com>
2026-01-12 20:22:41 +0900

    controller 구현

--- src/main/java/com/example/gujeuck_server/domain/admin/presentation/AdminController.java
+++ src/main/java/com/example/gujeuck_server/domain/admin/presentation/AdminController.java
@@ -14,6 +14,7 @@ import com.example.gujeuck_server.domain.user.presentation.dto.response.UserDto;
 import com.example.gujeuck_server.domain.user.presentation.dto.response.UserResponse;
 import com.example.gujeuck_server.domain.user.service.QueryUserListByResidenceService;
 import com.example.gujeuck_server.domain.user.service.QueryUserListService;
+import com.example.gujeuck_server.domain.user.service.UpdateUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -22,6 +23,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RestController
 @RequiredArgsConstructor
@@ -34,6 +37,7 @@ public class AdminController {
     private final ReissueService reissueService;
     private final QueryUserListService queryUserListService;
     private final QueryUserListByResidenceService queryUserListByResidenceService;
+    private final UpdateUserService updateUserService;
 
     @GetMapping("/user/all")
     public SliceWithTotalResponse<UserDto> getAllUserList(
@@ -48,7 +52,8 @@ public class AdminController {
     }
 
     @PatchMapping("/user/{id}")
-    public void updateUser(@RequestBody @Valid UpdateUserRequest request) {
+    public void updateUser(@PathVariable Long id, @RequestBody @Valid UpdateUserRequest request) {
+        updateUserService.execute(id, request);
     }
 
     @PostMapping("/login")
