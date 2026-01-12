6d1d9676236e61c004b9e620bdc77c5c9ed2f052
oijwef098234 <oijwef098234@gmail.com>
2026-01-12 23:08:00 +0900

    feat :: 방문목적 순서 변경

--- src/main/java/com/example/gujeuck_server/domain/purpose/domain/repository/PurposeRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/purpose/domain/repository/PurposeRepository.java
@@ -2,6 +2,7 @@ package com.example.gujeuck_server.domain.purpose.domain.repository;
 
 import com.example.gujeuck_server.domain.purpose.domain.Purpose;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +14,7 @@ public interface PurposeRepository extends JpaRepository<Purpose, Long> {
     List<Purpose> findAllByOrderByPurposeIndexAsc();
 
     List<Purpose> findAllByPurposeIndexGreaterThan(int purposeIndex);
+
+    @Query("select coalesce(max(p.purposeIndex), 0) from Purpose p")
+    int findMaxPurposeIndex();
 }
--- src/main/java/com/example/gujeuck_server/domain/purpose/service/CreatePurposeService.java
+++ src/main/java/com/example/gujeuck_server/domain/purpose/service/CreatePurposeService.java
@@ -1,15 +1,12 @@
 package com.example.gujeuck_server.domain.purpose.service;
 
 import com.example.gujeuck_server.domain.admin.facade.AdminFacade;
-import com.example.gujeuck_server.domain.purpose.exception.PurposeAlreadyExistException;
 import com.example.gujeuck_server.domain.purpose.presentation.dto.request.PurposeRequest;
 import com.example.gujeuck_server.domain.purpose.domain.Purpose;
 import com.example.gujeuck_server.domain.purpose.domain.repository.PurposeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,18 +16,15 @@ public class CreatePurposeService {
 
     @Transactional
     public void createPurpose(PurposeRequest request) {
-
         adminFacade.currentUser();
 
-        if (purposeRepository.findByPurpose(request.getPurpose()).isPresent()) {
-            throw PurposeAlreadyExistException.EXCEPTION;
-        }
+        int nextIndex = purposeRepository.findMaxPurposeIndex() + 1;
 
-        List<Purpose> purposes = purposeRepository.findAll();
-
-        purposeRepository.save(Purpose.builder()
-                .purpose(request.getPurpose())
-                .purposeIndex(purposes.size() + 1)
-                .build());
+        purposeRepository.save(
+                Purpose.builder()
+                        .purpose(request.getPurpose())
+                        .purposeIndex(nextIndex)
+                        .build()
+        );
     }
 }
