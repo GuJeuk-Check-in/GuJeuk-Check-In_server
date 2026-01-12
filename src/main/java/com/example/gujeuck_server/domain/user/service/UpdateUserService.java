42a1a7490525901a43f443e4830670a21510c0c5
YoonJiwon <gaden119000@gmail.com>
2026-01-12 15:41:34 +0900

    Service 추가

---
+++ src/main/java/com/example/gujeuck_server/domain/user/service/UpdateUserService.java
@@ -0,0 +1,12 @@
+package com.example.gujeuck_server.domain.user.service;
+
+import com.example.gujeuck_server.domain.user.presentation.dto.request.UpdateUserRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UpdateUserService {
+
+    public void execute(UpdateUserRequest request) {
+
+    }
+}
