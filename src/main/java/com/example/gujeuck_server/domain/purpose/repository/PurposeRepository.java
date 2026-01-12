b0943af5c6b2cb7d8ccbd2953820f1b21231b439
oijwef098234 <oijwef098234@gmail.com>
2026-01-12 14:41:24 +0900

    feat :: 중복된 purposeIndex가 생성되는 에러 해결

--- src/main/java/com/example/gujeuck_server/domain/purpose/repository/PurposeRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/purpose/repository/PurposeRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface PurposeRepository extends JpaRepository<Purpose, Long> {
     List<Purpose> findByPurpose(String purpose);
     List<Purpose> findAllByOrderByPurposeIndexAsc();
+    List<Purpose> findAllByPurposeIndexGreaterThan(int purposeIndex);
 }
