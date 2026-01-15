43cc23e5c14f90a56551a45aa9220c25b595ba02
kn4tti <gaden119000@gmail.com>
2026-01-15 17:10:07 +0900

    디자인에 맞게 수정

--- src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/request/UpdateUserRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/request/UpdateUserRequest.java
@@ -1,20 +1,20 @@
 package com.example.gujeuck_server.domain.user.presentation.dto.request;
 
-import com.example.gujeuck_server.domain.user.domain.enums.Gender;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record UpdateUserRequest(
         @NotBlank
         String name,
+
         @NotBlank
         String userId,
+
         @NotBlank
         String phone,
-        @NotNull
-        Gender gender,
+
         @NotBlank
         String birthYMD,
+
         @NotBlank
         String residence
 ) {
