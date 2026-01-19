e3a4e2eedc0b4a222858065820cb548a491e2b52
박태수 <taesupark0504@gmail.com>
2026-01-19 09:27:03 +0900

    chore :: conflict class

--- src/main/java/com/example/gujeuck_server/domain/user/service/SignupService.java
+++ src/main/java/com/example/gujeuck_server/domain/user/service/SignupService.java
@@ -9,7 +9,7 @@ import com.example.gujeuck_server.domain.user.domain.enums.Age;
 import com.example.gujeuck_server.domain.user.exception.ExistUserIdException;
 import com.example.gujeuck_server.domain.user.domain.repository.UserRepository;
 import com.example.gujeuck_server.domain.user.presentation.dto.request.SignupRequest;
-import com.example.gujeuck_server.domain.user.presentation.dto.response.SignupResponse;
+import com.example.gujeuck_server.domain.user.presentation.dto.response.SignUpResponse;
 import com.example.gujeuck_server.global.utility.CalculateAgeService;
 import com.example.gujeuck_server.global.utility.DateFormatter;
 import lombok.RequiredArgsConstructor;
@@ -32,9 +32,9 @@ public class SignupService {
     private static final String TIME = "HH:mm";
 
     @Transactional
-    public SignupResponse signup(SignupRequest request) {
+    public SignUpResponse signup(SignupRequest request) {
 
-        SignupResponse signupResponse = createUserId(request.getName(), request.getBirthYMD());
+        SignUpResponse signupResponse = createUserId(request.getName(), request.getBirthYMD());
 
         Age age = calculateAgeService.getAge(request.getBirthYMD());
 
@@ -48,7 +48,7 @@ public class SignupService {
 
         String resolvedResidence = User.resolveResidence(request.getResidence());;
 
-        User user = createUser(request, age, signupResponse.userId(), resolvedResidence);
+        User user = createUser(request, age, signupResponse.getUserId(), resolvedResidence);
 
         user.increaseCount();
 
@@ -61,7 +61,7 @@ public class SignupService {
         return signupResponse;
     }
 
-    private SignupResponse createUserId(String name, String birthYMD) {
+    private SignUpResponse createUserId(String name, String birthYMD) {
 
         String userId = User.generateUserId(name, birthYMD);
 
@@ -69,7 +69,7 @@ public class SignupService {
             throw ExistUserIdException.EXCEPTION;
         }
 
-        return SignupResponse.builder()
+        return SignUpResponse.builder()
                 .userId(userId)
                 .build();
     }
