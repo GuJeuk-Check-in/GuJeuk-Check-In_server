31fae17bbf83034fe1211deefaeea6324303f754
박태수 <taesupark0504@gmail.com>
2026-01-23 22:51:06 +0900

    refactor :: v2 migration file organ 제약조건 맞추기

--- src/main/resources/db/migration/V2__create_organ_table.sql
+++ src/main/resources/db/migration/V2__create_organ_table.sql
@@ -2,10 +2,9 @@
     organ 테이블 생성
  */
 CREATE TABLE organ (
-                       id BIGINT PRIMARY KEY,
-                       organ_name VARCHAR(255) NOT NULL,
-                       password VARCHAR(100) NOT NULL
-);
+                       id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                       organ_name VARCHAR(255) NOT NULL UNIQUE,
+                       password VARCHAR(300) NOT NULL);
 
 /*
  organName UNIQUE 제약 조건
