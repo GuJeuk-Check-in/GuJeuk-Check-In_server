61d8627b6e9ce6248a3c7f322958d1eac532918a
박태수 <taesupark0504@gmail.com>
2026-01-23 22:40:51 +0900

    refactor :: 비밀번호 일치 불일치 검증

--- src/main/java/com/example/gujeuck_server/domain/organ/service/LoginOrganService.java
+++ src/main/java/com/example/gujeuck_server/domain/organ/service/LoginOrganService.java
@@ -6,8 +6,10 @@ import com.example.gujeuck_server.domain.organ.presentation.dto.response.TokenRe
 import com.example.gujeuck_server.domain.organ.domain.Organ;
 import com.example.gujeuck_server.domain.organ.exception.OrganNotFoundException;
 import com.example.gujeuck_server.domain.organ.domain.repository.OrganRepository;
+import com.example.gujeuck_server.domain.user.exception.PasswordMismatchException;
 import com.example.gujeuck_server.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,12 +18,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class LoginOrganService {
     private final OrganRepository organRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public TokenResponse execute(LoginOrganRequest request) {
 
         Organ organ = organRepository.findByOrganName(request.getOrganName())
                 .orElseThrow(() -> OrganNotFoundException.EXCEPTION);
+
+        if (!passwordEncoder.matches(request.getPassword(), organ.getPassword())) {
+            throw PasswordMismatchException.EXCEPTION;
+        }
 
         Client client = Client.valueOf(request.getClient().toUpperCase());
 
