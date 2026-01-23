4c0627e6ad55567ae4a51399e3d284caa27464d7
박태수 <taesupark0504@gmail.com>
2026-01-23 22:20:27 +0900

    refactor :: cors 허용 경로 설정

--- src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
+++ src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/**", "/organ/**", "/purpose/**", "/log/**").permitAll()
+                        .requestMatchers("/user/sign-up", "user/login", "/organ/create").permitAll()
                         .anyRequest().authenticated()
                 )
                 .with(new SecurityFilterConfig(jwtTokenProvider, objectMapper), Customizer.withDefaults())
