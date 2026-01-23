d037e1bad6ab1cdd5ef7f11fe14b6f0c9adfab63
박태수 <taesupark0504@gmail.com>
2026-01-23 19:19:50 +0900

    refactor :: password 저장 시 암호화

--- src/main/java/com/example/gujeuck_server/domain/organ/service/CreateOrganService.java
+++ src/main/java/com/example/gujeuck_server/domain/organ/service/CreateOrganService.java
@@ -4,6 +4,7 @@ import com.example.gujeuck_server.domain.organ.presentation.dto.request.OrganReq
 import com.example.gujeuck_server.domain.organ.domain.Organ;
 import com.example.gujeuck_server.domain.organ.domain.repository.OrganRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CreateOrganService {
     private final OrganRepository organRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public void execute(OrganRequest request) {
+
         organRepository.save(Organ.builder()
                 .organName(request.getOrganName())
-                .password(request.getPassword())
+                .password(passwordEncoder.encode(request.getPassword()))
                 .build());
     }
 }
