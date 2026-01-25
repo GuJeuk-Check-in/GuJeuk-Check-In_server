63b1e4757634d2409bb7fcd5ae14c86840fbca96
박태수 <taesupark0504@gmail.com>
2026-01-25 10:03:38 +0900

    feat :: findAllByVisitDate query DSL 최적화

--- src/main/java/com/example/gujeuck_server/domain/log/domain/repository/LogRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/log/domain/repository/LogRepository.java
@@ -5,11 +5,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface LogRepository extends JpaRepository<Log, Long>, LogRepositoryCustom {
 
   Slice<Log> findAllBy(Pageable pageable);
-
-  List<Log> findAllByVisitDateStartingWith(String visitDatePrefix);
 }
