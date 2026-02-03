6dc9136ba8c4f62b5465c17bec8612b81a012fea
oijwef098234 <oijwef098234@gmail.com>
2026-02-03 16:57:19 +0900

    feat :: ResidenceController 생성

---
+++ src/main/java/com/example/gujeuck_server/domain/residence/presentation/ResidenceController.java
@@ -0,0 +1,12 @@
+package com.example.gujeuck_server.domain.residence.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/residence")
+public class ResidenceController {
+    
+}
