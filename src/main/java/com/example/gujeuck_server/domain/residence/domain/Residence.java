832ee233c7c23a82807b2dcdbedd685d33139577
oijwef098234 <oijwef098234@gmail.com>
2026-02-03 16:38:16 +0900

    feat :: 거주지 Entity 작성

---
+++ src/main/java/com/example/gujeuck_server/domain/residence/domain/Residence.java
@@ -0,0 +1,20 @@
+package com.example.gujeuck_server.domain.residence.domain;
+
+import com.example.gujeuck_server.domain.organ.domain.Organ;
+import com.example.gujeuck_server.global.entity.BaseIdEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Residence extends BaseIdEntity {
+    @Column(name = "residence", nullable = false, length = 30)
+    private String residenceName;
+
+    @Column(nullable = false)
+    private int residenceIndex;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "organ_id", nullable = false)
+    private Organ organ;
+}
