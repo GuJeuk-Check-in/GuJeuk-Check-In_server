09111c58e585878350209586fd1adb0131d2047a
oijwef098234 <oijwef098234@gmail.com>
2026-02-05 22:49:31 +0900

    feat :: 불필요한 어노테이션 제거

--- src/main/java/com/example/gujeuck_server/domain/residence/presentation/dto/request/ResidenceMoveRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/residence/presentation/dto/request/ResidenceMoveRequest.java
@@ -1,13 +1,11 @@
 package com.example.gujeuck_server.domain.residence.presentation.dto.request;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
 @Setter
-@Builder
 @Getter
 public class ResidenceMoveRequest {
     private List<Long> residenceId;
