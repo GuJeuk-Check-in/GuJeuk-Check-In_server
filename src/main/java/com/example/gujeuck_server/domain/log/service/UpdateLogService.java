a22a751aa8fe65f84d42c5fa4d058f9e15273a01
박태수 <taesupark0504@gmail.com>
2026-02-26 00:00:06 +0900

    chore :: purpose 관련 잡것 제거

--- src/main/java/com/example/gujeuck_server/domain/log/service/UpdateLogService.java
+++ src/main/java/com/example/gujeuck_server/domain/log/service/UpdateLogService.java
@@ -1,12 +1,10 @@
 package com.example.gujeuck_server.domain.log.service;
 
 import com.example.gujeuck_server.domain.organ.domain.Organ;
-import com.example.gujeuck_server.domain.log.exception.LogAccessDeniedException;
 import com.example.gujeuck_server.domain.log.facade.LogFacade;
 import com.example.gujeuck_server.domain.log.presentation.dto.request.LogRequest;
 import com.example.gujeuck_server.domain.organ.facade.OrganFacade;
 import com.example.gujeuck_server.domain.log.domain.Log;
-import com.example.gujeuck_server.domain.purpose.facade.PurposeFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 public class UpdateLogService {
     private final LogFacade logFacade;
     private final OrganFacade organFacade;
-    private final PurposeFacade purposeFacade;
 
     @Transactional
     public void execute(Long logId, LogRequest request) {
