8d0d0be362e9dfc3b4c5a47c6d613dcb86739a00
박태수 <taesupark0504@gmail.com>
2026-03-04 19:11:53 +0900

    fix :: 거주지 별 조회 기타 query 변경

--- src/main/java/com/example/gujeuck_server/domain/residence/domain/repository/ResidenceRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/residence/domain/repository/ResidenceRepository.java
@@ -2,6 +2,7 @@ package com.example.gujeuck_server.domain.residence.domain.repository;
 
 import com.example.gujeuck_server.domain.residence.domain.Residence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ public interface ResidenceRepository extends JpaRepository<Residence, Long>, Res
 
     List<Residence> findAllByOrganIdAndResidenceIndexGreaterThan(Long organId, int residenceIndex);
 
+    @Query("select r.residenceName from Residence r where r.organ.id = :organId")
     List<String> findAllResidenceNameByOrganId(Long organId);
 
     Optional<Residence> findByResidenceNameAndOrganId(String residenceName, Long organId);
