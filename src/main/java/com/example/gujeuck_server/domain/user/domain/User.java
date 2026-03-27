16efad711fff19150d7d5ba878af25ad7b3f6013
oijwef098234 <oijwef098234@gmail.com>
2026-03-27 17:21:23 +0900

    feat: 회원목록 수정 메서드에 Gender항목 추가

--- src/main/java/com/example/gujeuck_server/domain/user/domain/User.java
+++ src/main/java/com/example/gujeuck_server/domain/user/domain/User.java
@@ -49,12 +49,13 @@ public class User extends BaseIdEntity {
     @JoinColumn(name = "organ_id", nullable = false)
     private Organ organ;
 
-    public void updateUser(String name, String userId, String phone, String birthYMD, String residence) {
+    public void updateUser(String name, String userId, String phone, String birthYMD, String residence, Gender gender) {
         this.name = name;
         this.userId = userId;
         this.phone = phone;
         this.birthYMD = birthYMD;
         this.residence = residence;
+        this.gender = gender;
     }
 
     public static String generateUserId(String name, String birthYMD) {
