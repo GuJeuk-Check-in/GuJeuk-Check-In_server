115587f4b7fa2bae046de2d92d7ad973b1550b17
oijwef098234 <oijwef098234@gmail.com>
2026-03-27 17:13:46 +0900

    feat: 회원 목록 수정 요청 DTO 성별 필드 추가

--- src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/request/UpdateUserRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/request/UpdateUserRequest.java
@@ -1,6 +1,7 @@
 package com.example.gujeuck_server.domain.user.presentation.dto.request;
 
 import com.example.gujeuck_server.domain.residence.domain.Residence;
+import com.example.gujeuck_server.domain.user.domain.enums.Gender;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -16,6 +17,8 @@ public record UpdateUserRequest(
         @NotBlank(message = "전화번호를 입력해주세요.")
         @Size(max = 11, message = "전화번호는 11자리 이상으로 입력할 수 없습니다.")
         String phone,
+        
+        Gender gender,
 
         @NotBlank
         String birthYMD,
