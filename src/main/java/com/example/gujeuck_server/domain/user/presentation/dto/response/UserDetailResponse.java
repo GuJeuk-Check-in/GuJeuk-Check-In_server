ed610fda8e570508c4d584e982930b98505ad19d
oijwef098234 <oijwef098234@gmail.com>
2026-03-27 19:38:31 +0900

    hotifx :: 회원정보 상세조회 성별 필드 추가

--- src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/response/UserDetailResponse.java
+++ src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/response/UserDetailResponse.java
@@ -1,16 +1,26 @@
 package com.example.gujeuck_server.domain.user.presentation.dto.response;
 
 import com.example.gujeuck_server.domain.user.domain.User;
+import com.example.gujeuck_server.domain.user.domain.enums.Gender;
 
 public record UserDetailResponse(
         Long id,
         String name,
         String userId,
+        Gender gender,
         String phone,
         String birthYMD,
         String residence
 ) {
     public static UserDetailResponse from(User user) {
-        return new UserDetailResponse(user.getId(), user.getName(), user.getUserId(), user.getPhone(), user.getBirthYMD(), user.getResidence());
+        return new UserDetailResponse(
+                user.getId(),
+                user.getName(),
+                user.getUserId(),
+                user.getGender(),
+                user.getPhone(),
+                user.getBirthYMD(),
+                user.getResidence()
+        );
     }
 }
