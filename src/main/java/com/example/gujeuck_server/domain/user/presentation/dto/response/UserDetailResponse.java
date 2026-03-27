d204891591f57eaa9d2df6c6de01a44dc7cc17b1
oijwef098234 <oijwef098234@gmail.com>
2026-03-27 19:16:46 +0900

    hotfix :: 회원정보 상세조회에서 ID 보내주기

--- src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/response/UserDetailResponse.java
+++ src/main/java/com/example/gujeuck_server/domain/user/presentation/dto/response/UserDetailResponse.java
@@ -3,7 +3,7 @@ package com.example.gujeuck_server.domain.user.presentation.dto.response;
 import com.example.gujeuck_server.domain.user.domain.User;
 
 public record UserDetailResponse(
-
+        Long id,
         String name,
         String userId,
         String phone,
@@ -11,6 +11,6 @@ public record UserDetailResponse(
         String residence
 ) {
     public static UserDetailResponse from(User user) {
-        return new UserDetailResponse(user.getName(), user.getUserId(), user.getPhone(), user.getBirthYMD(), user.getResidence());
+        return new UserDetailResponse(user.getId(), user.getName(), user.getUserId(), user.getPhone(), user.getBirthYMD(), user.getResidence());
     }
 }
