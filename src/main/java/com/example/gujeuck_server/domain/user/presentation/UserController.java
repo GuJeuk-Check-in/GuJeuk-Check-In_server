79998fd484e21676deed32396411f3eb29962c8c
oijwef098234 <oijwef098234@gmail.com>
2026-06-28 21:39:32 +0900

    feat(#79) :: 유저 확인 로직 및 체크인 로직 controller 연결

--- src/main/java/com/example/gujeuck_server/domain/user/presentation/UserController.java
+++ src/main/java/com/example/gujeuck_server/domain/user/presentation/UserController.java
@@ -1,10 +1,12 @@
 package com.example.gujeuck_server.domain.user.presentation;
 
-import com.example.gujeuck_server.domain.log.presentation.dto.request.LogRequest;
-import com.example.gujeuck_server.domain.log.service.CreateLogService;
-import com.example.gujeuck_server.domain.organ.facade.OrganFacade;
 import com.example.gujeuck_server.domain.user.presentation.dto.request.SignupRequest;
+import com.example.gujeuck_server.domain.user.presentation.dto.request.UserCheckInRequest;
+import com.example.gujeuck_server.domain.user.presentation.dto.request.UserExistsRequest;
+import com.example.gujeuck_server.domain.user.presentation.dto.response.UserExistsResponse;
 import com.example.gujeuck_server.domain.user.service.SignupService;
+import com.example.gujeuck_server.domain.user.service.UserCheckInService;
+import com.example.gujeuck_server.domain.user.service.UserExistsService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,8 +19,13 @@ public class UserController {
     private static final Long DEFAULT_CHECK_IN_ORGAN_ID = 1L;
 
     private final SignupService signupService;
-    private final CreateLogService createLogService;
-    private final OrganFacade organFacade;
+    private final UserExistsService userExistsService;
+    private final UserCheckInService userCheckInService;
+
+    @PostMapping()
+    public UserExistsResponse existsUser(@RequestBody @Valid UserExistsRequest request) {
+        return userExistsService.execute(request);
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/sign-up")
@@ -28,7 +35,7 @@ public class UserController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/check-in")
-    public void checkIn(@RequestBody @Valid LogRequest request) {
-        createLogService.execute(organFacade.getOrganById(DEFAULT_CHECK_IN_ORGAN_ID), request);
+    public void checkIn(@RequestBody @Valid UserCheckInRequest request) {
+        userCheckInService.execute(request);
     }
 }
