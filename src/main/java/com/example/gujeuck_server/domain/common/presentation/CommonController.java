725a6fa4ddf8b2578538eb19e337f962bba5ee24
hyemin <ohyemin1113@gmail.com>
2026-07-20 08:31:28 +0900

    fix :: 엔드포인트 수정

--- src/main/java/com/example/gujeuck_server/domain/common/presentation/CommonController.java
+++ src/main/java/com/example/gujeuck_server/domain/common/presentation/CommonController.java
@@ -3,13 +3,14 @@ package com.example.gujeuck_server.domain.common.presentation;
 import com.example.gujeuck_server.domain.common.service.HealthCheckService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/common")
+@RestController
+@RequestMapping("/common")
 @RequiredArgsConstructor
 public class CommonController {
     private final HealthCheckService healthCheckService;
-
 
     @GetMapping("/health")
     public String health() {
