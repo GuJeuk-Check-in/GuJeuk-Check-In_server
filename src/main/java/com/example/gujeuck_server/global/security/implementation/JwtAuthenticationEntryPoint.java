1d3d9b5c971c8f9d45bfd00f38beeb542d6aa6c2
oijwef098234 <oijwef098234@gmail.com>
2026-07-29 16:49:25 +0900

    refeactor(#92) :: AuthenticationEntryPoint구현체 구현

--- src/main/java/com/example/gujeuck_server/global/security/implementation/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/gujeuck_server/global/security/implementation/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.example.gujeuck_server.global.security.implementation;
 
+import com.example.gujeuck_server.global.error.ErrorResponse;
+import com.example.gujeuck_server.global.error.exception.ErrorCode;
 import com.example.gujeuck_server.global.security.jwt.JwtTokenProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -9,14 +11,19 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     private final ObjectMapper objectMapper;
 
     @Override
-    public void execute(HttpServletResponse response, HttpServletRequest request, AuthenticationException authException) {
-        
+    public void execute(HttpServletResponse response, HttpServletRequest request, AuthenticationException authException) throws IOException {
+        ErrorCode errorCode = ErrorCode.TOKEN_NOT_FOUND;
+        response.setStatus(errorCode.getStatusCode());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        objectMapper.writeValue(response.getWriter(), ErrorResponse.of(errorCode, errorCode.getErrorMessage()));
     }
-
 }
