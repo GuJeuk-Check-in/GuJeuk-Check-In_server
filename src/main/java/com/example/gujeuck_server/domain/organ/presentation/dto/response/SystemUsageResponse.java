81988ee4245aed0963cc863dc4f67754682b82ea
oijwef098234 <oijwef098234@gmail.com>
2026-08-07 01:40:42 +0900

    feat(#103) :: 시설 이용률 반환형 객체 구현

--- src/main/java/com/example/gujeuck_server/domain/organ/presentation/dto/response/SystemUsageResponse.java
+++ src/main/java/com/example/gujeuck_server/domain/organ/presentation/dto/response/SystemUsageResponse.java
@@ -1,11 +1,39 @@
 package com.example.gujeuck_server.domain.organ.presentation.dto.response;
 
-public record SystemUsageResponse(
-    int total,
-    int january,
-    int february,
-    int march,
-    int april,
+import lombok.Builder;
 
-) {
+import java.util.List;
+
+@Builder
+public record SystemUsageResponse(
+    SystemUsageOneResponse total,
+    SystemUsageOneResponse january,
+    SystemUsageOneResponse february,
+    SystemUsageOneResponse march,
+    SystemUsageOneResponse april,
+    SystemUsageOneResponse may,
+    SystemUsageOneResponse june,
+    SystemUsageOneResponse july,
+    SystemUsageOneResponse august,
+    SystemUsageOneResponse september,
+    SystemUsageOneResponse october,
+    SystemUsageOneResponse november,
+    SystemUsageOneResponse december
+    ) {
+    public static SystemUsageResponse of(List<SystemUsageOneResponse> systemUsageOneResponses) {
+        return SystemUsageResponse.builder()
+            .total(systemUsageOneResponses.get(0))
+            .january(systemUsageOneResponses.get(1))
+            .february(systemUsageOneResponses.get(2))
+            .march(systemUsageOneResponses.get(3))
+            .april(systemUsageOneResponses.get(4))
+            .may(systemUsageOneResponses.get(5))
+            .june(systemUsageOneResponses.get(6))
+            .july(systemUsageOneResponses.get(7))
+            .august(systemUsageOneResponses.get(8))
+            .september(systemUsageOneResponses.get(9))
+            .october(systemUsageOneResponses.get(10))
+            .december(systemUsageOneResponses.get(11))
+            .build();
+    }
 }
