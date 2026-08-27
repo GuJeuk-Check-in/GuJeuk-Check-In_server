c1d216626a3945ef3bed3c6b57c32895c2b73748
oijwef098234 <oijwef098234@gmail.com>
2026-08-27 10:04:08 +0900

    refactor(#113) :: 예외처리 코드 스타일 통일화

--- src/main/java/com/example/gujeuck_server/domain/log/exception/ClientRecordIdMismatchException.java
+++ src/main/java/com/example/gujeuck_server/domain/log/exception/ClientRecordIdMismatchException.java
@@ -7,6 +7,6 @@ public class ClientRecordIdMismatchException extends GujeukException {
     public static final GujeukException EXCEPTION = new ClientRecordIdMismatchException();
 
     private ClientRecordIdMismatchException() {
-        super(ErrorCode.CLIENT_RECORD_ID_MISMATCH, "이미 처리된 clientRecordId의 요청 데이터와 현재 요청 데이터가 일치하지 않습니다.");
+        super(ErrorCode.CLIENT_RECORD_ID_MISMATCH);
     }
 }
