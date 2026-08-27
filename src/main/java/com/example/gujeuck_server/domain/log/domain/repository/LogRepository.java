9d4a7a5a4ae0cabdad2654a3918882776cefef1b
oijwef098234 <oijwef098234@gmail.com>
2026-08-27 18:33:21 +0900

    refactor(#113) :: 불필요한 코드 제거
    정욱이가 처음써보는 설명

--- src/main/java/com/example/gujeuck_server/domain/log/domain/repository/LogRepository.java
+++ src/main/java/com/example/gujeuck_server/domain/log/domain/repository/LogRepository.java
@@ -6,14 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface LogRepository extends JpaRepository<Log, Long>, LogRepositoryCustom {
 
   Slice<Log> findAllByOrganId(Pageable pageable, Long organId);
-
-  List<Log> findAllByOrganIdAndVisitDateStartingWith(Long organId, String visitDate);
 
   Slice<Log> findAllByOrganIdAndVisitDateStartingWith(Long organId, String visitDate, Pageable pageable);
 
