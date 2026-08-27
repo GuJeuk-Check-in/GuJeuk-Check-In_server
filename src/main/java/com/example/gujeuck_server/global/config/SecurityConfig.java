3f9f20eae2d1e4be31d8da791593a1ed7f43ee87
oijwef098234 <oijwef098234@gmail.com>
2026-08-27 09:57:31 +0900

    refactor(#113) :: 불필요한 yml 인자 제거

--- src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
+++ src/main/java/com/example/gujeuck_server/global/config/SecurityConfig.java
@@ -35,9 +35,6 @@ public class SecurityConfig {
     @Value("${cors.allowed-origins.stag-url}")
     private String stagUrl;
 
-    @Value("${cors.allowed-origins.vercel-url}")
-    private String vercelUrl;
-
     @Value("${cors.allowed-origins.test-url}")
     private String testUrl;
 
@@ -100,7 +97,6 @@ public class SecurityConfig {
 
         addOrigins(origins, prodUrl);
         addOrigins(origins, stagUrl);
-        addOrigins(origins, vercelUrl);
         addOrigins(origins, testUrl);
 
         return origins;
