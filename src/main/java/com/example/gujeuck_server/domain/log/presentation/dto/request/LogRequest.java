2dca7d1368ea1df1d8f846a9b78c1c5fb2ca199f
oijwef098234 <oijwef098234@gmail.com>
2026-08-27 19:06:49 +0900

    refactor(#113) :: LogRequest 리펙터링
    class -> record, @Size 유효성 검사 어노테이션 설정

--- src/main/java/com/example/gujeuck_server/domain/log/presentation/dto/request/LogRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/log/presentation/dto/request/LogRequest.java
@@ -2,37 +2,40 @@ package com.example.gujeuck_server.domain.log.presentation.dto.request;
 
 import com.example.gujeuck_server.domain.user.domain.enums.Age;
 import jakarta.validation.constraints.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-public class LogRequest {
+public record LogRequest (
 
     @NotBlank(message = "이름은 필수 입력 항목입니다.")
-    @Size(max = 30, message = "이름은 30자 이하로 입력해주세요.")
-    private String name;
+    @Size(max = 10, message = "이름은 10자 이하로 입력해주세요.")
+    String name,
 
     @NotNull(message = "나이를 선택해주세요.")
-    private Age age;
+    @Size(max = 6, message = "나이 입력값이 유효하지 않습니다.")
+    Age age,
 
-    private String phone;
+    @NotBlank(message = "전화번호를 입력해주세요.")
+    @Size(max = 15, message = "전화번호를 15자 미만으로 입력해주세요.")
+    String phone,
 
     @NotNull(message = "남자 동행인 수는 null일 수 없습니다.")
-    private int maleCount;
+    int maleCount,
 
     @NotNull(message = "여자 동행인 수는 null일 수 없습니다.")
-    private int femaleCount;
+    int femaleCount,
 
     @NotBlank(message = "방문 목적을 선택해주세요.")
-    private String purpose;
+    @Size(max = 30, message = "방문 목적이 유효하지 않습니다.")
+    String purpose,
 
     @AssertTrue(message = "개인정보 수집 및 이용 동의를 체크해주세요.")
-    private boolean privacyAgreed;
+    boolean privacyAgreed,
 
     @NotBlank(message = "방문일자를 비워둘 수 없습니다.")
-    private String visitDate;
+    @Size(max = 11, message = "날짜 형식이 유효하지 않습니다.")
+    String visitDate,
 
     @NotBlank(message = "방문시각을 비워둘 수 없습니다.")
-    private String visitTime;
+    @Size(max = 5, message = "방문 시각이 유효하지 않습니다.")
+    String visitTime
+) {
 }
