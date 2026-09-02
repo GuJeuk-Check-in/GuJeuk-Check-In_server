cc1fa58bd9cef1d1445f114f04305b2d2d440b8d
oijwef098234 <oijwef098234@gmail.com>
2026-09-02 20:19:11 +0900

    feat(#113) :: 거주지 순서 변경 요청에 검증을 붙인다

--- src/main/java/com/example/gujeuck_server/domain/residence/presentation/dto/request/ResidenceMoveRequest.java
+++ src/main/java/com/example/gujeuck_server/domain/residence/presentation/dto/request/ResidenceMoveRequest.java
@@ -1,6 +1,11 @@
 package com.example.gujeuck_server.domain.residence.presentation.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
+
 import java.util.List;
 
-public record ResidenceMoveRequest(List<Long> residenceId) {
+public record ResidenceMoveRequest(
+    @NotEmpty(message = "방문 목적 순서가 유효하지 않습니다.")
+    List<Long> residenceId
+) {
 }
